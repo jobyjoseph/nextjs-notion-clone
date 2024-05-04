606c9c0bf75338bb95859053cd9de8c8964748b1
Joby Joseph <reachjoby@gmail.com>
2024-05-04 00:47:20 -0700

    Footer done

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -5,7 +5,6 @@ export const Heroes = () => {
     <div className="flex flex-col items-center justify-center max-w-5xl">
       <div className="flex items-center">
         <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h=[350px] md:h-[400px] md:w-[400px]">
-          <h1>hello</h1>
           <Image
             src="/document-light.png"
             fill
@@ -13,7 +12,7 @@ export const Heroes = () => {
             alt="Documents"
           />
         </div>
-        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h=[350px] md:h-[400px] md:w-[400px]">
+        <div className="relative w-[300px] h-[300px] hidden md:block">
           <Image
             src="/reading-light.png"
             fill
---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
+export const Logo = () => {
+  return <p className={cn("font-semibold", font.className)}>Jotion</p>;
+};

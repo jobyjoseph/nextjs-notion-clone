02e67705fe01129662417a46bc309c79dd9fe79a
Joby Joseph <reachjoby@gmail.com>
2024-05-12 12:10:14 -0700

    sidebar in progress

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -16,14 +16,39 @@ export const Navigation = () => {
   const [isResetting, setIsResetting] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
-  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const handleMouseDown = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
     event.preventDefault();
     event.stopPropagation();
 
     isResizingRef.current = true;
-    document.addEventListener("mousemove",handleMouseMove);
+    document.addEventListener("mousemove", handleMouseMove);
     document.addEventListener("mouseup", handleMouseUp);
-  }
+  };
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (!isResizingRef.current) return;
+    let newWidth = event.clientX;
+
+    if (newWidth < 240) newWidth = 240;
+    if (newWidth > 480) newWidth = 480;
+
+    if (sidebarRef.current && navbarRef.current) {
+      sidebarRef.current.style.width = `${newWidth}px`;
+      navbarRef.current.style.setProperty("left", `${newWidth}px`);
+      navbarRef.current.style.setProperty(
+        "width",
+        `calc(100% - ${newWidth}px)`
+      );
+    }
+  };
+
+  const handleMouseUp = () => {
+    isResizingRef.current = false;
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
+  };
 
   return (
     <>
@@ -50,7 +75,11 @@ export const Navigation = () => {
         <div className="mt-4">
           <p>Documents</p>
         </div>
-        <div onMouseDown={() => {}} onClick={() => {}} className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+        <div
+          onMouseDown={() => {}}
+          onClick={() => {}}
+          className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
+        />
       </aside>
       <div
         ref={navbarRef}
@@ -61,7 +90,9 @@ export const Navigation = () => {
         )}
       >
         <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && <MenuIcon className="h-6 w-6 text-muted-foreground"}
+          {isCollapsed && (
+            <MenuIcon className="h-6 w-6 text-muted-foreground" />
+          )}
         </nav>
       </div>
     </>

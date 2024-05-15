491fd20cad36d4d0bf59f6d68adfcb1def9da422
Joby Joseph <reachjoby@gmail.com>
2024-05-15 04:47:16 -0700

    Avatar component added

--- app/(main)/_components/user-item.tsx
+++ app/(main)/_components/user-item.tsx
@@ -1,5 +1,36 @@
 "use client";
 
+import { ChevronsLeftRight } from "lucide-react";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { useUser } from "@clerk/clerk-react";
+
 export const UserItem = () => {
-  return <div>User item</div>;
+  const { user } = useUser();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div
+          role="button"
+          className="flex items-center text-sm p-3 w-full hover:bg-primary/5"
+        >
+          <div className="gap-x-2 flex items-center max-w-[150px]">
+            <Avatar className="h-5 w-5">
+              <AvatarImage src={user?.imageUrl} />
+            </Avatar>
+            <span className="text-start font-medium line-clamp-1">
+              {user?.fullName}&apos;s Jotion
+            </span>
+          </div>
+        </div>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
 };

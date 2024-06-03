5f02f0a81b8c379289d383a247421d7d9eadfc86
Joby Joseph <reachjoby@gmail.com>
2024-06-03 11:50:30 -0700

    Trashbox component in progress

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -24,6 +24,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { TranshBox } from "./trash-box";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -159,7 +160,7 @@ export const Navigation = () => {
               className="p-0 w-72"
               side={isMobile ? "bottom" : "right"}
             >
-              <p>Trash box</p>
+              <TranshBox />
             </PopoverContent>
           </Popover>
         </div>

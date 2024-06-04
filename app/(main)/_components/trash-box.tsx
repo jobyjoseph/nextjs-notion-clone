2e94fab4365e64482a9105716f8653d7809c040b
Joby Joseph <reachjoby@gmail.com>
2024-06-04 08:52:26 -0700

    trash box in progress

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
+import { Input } from "@/components/ui/input";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
+import { Search } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
@@ -50,7 +53,28 @@ export const TranshBox = () => {
     if (params.documentId === documentId) {
       router.push("/documents");
     }
+
+    if (documents === undefined) {
+      return (
+        <div className="h-full flex items-center justify-center p-4">
+          <Spinner size="lg" />
+        </div>
+      );
+    }
   };
 
-  return <div>Trash box</div>;
+  return (
+    <div className="text-sm">
+      <div className="flex items-center gap-x-1 p-2">
+        <Search className="h-4 w-4" />
+        <Input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="h-7 px-2 focus-visible:ring-transparent bg-secondary"
+          placeholder="Filter by page title..."
+        />
+      </div>
+      <div className="mt-2 px-1 pb-1"></div>
+    </div>
+  );
 };

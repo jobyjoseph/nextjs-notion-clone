5c92121d8efe3848357cbad7bee52dca6719fe77
Joby Joseph <reachjoby@gmail.com>
2024-06-16 10:48:35 -0700

    Navbar in progress

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -1,10 +1,33 @@
 "use client";
 
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { useParams } from "next/navigation";
+
 interface NavbarProps {
   isCollapsed: boolean;
   onResetWidth: () => void;
 }
 
 export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
-  return <div>Navbar!</div>;
+  const params = useParams();
+
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId as Id<"documents">,
+  });
+
+  if (document === undefined) {
+    return <p>Loading...</p>;
+  }
+
+  if (document === null) {
+    return null;
+  }
+
+  return (
+    <>
+      <nav></nav>
+    </>
+  );
 };

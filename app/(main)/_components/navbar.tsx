a5456eac5c6e2629c18b7c15cba6fa972ccbd9d2
Joby Joseph <reachjoby@gmail.com>
2024-06-19 14:27:55 -0700

    Navbar in progress

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -3,6 +3,7 @@
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
+import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 
 interface NavbarProps {
@@ -27,7 +28,15 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   return (
     <>
-      <nav>Navbar here!</nav>
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
+        {isCollapsed && (
+          <MenuIcon
+            role="button"
+            onClick={onResetWidth}
+            className="h-6 w-6 text-muted-foreground"
+          />
+        )}
+      </nav>
     </>
   );
 };

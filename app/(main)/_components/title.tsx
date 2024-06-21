6f6816013f706c54da04290a5a3d8a2f49f88578
Joby Joseph <reachjoby@gmail.com>
2024-06-21 14:31:05 -0700

    Navbar document title in progress

--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -1,11 +1,47 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { useRef, useState } from "react";
 
 interface TitleProps {
   initialData: Doc<"documents">;
 }
 
 export const Title = ({ initialData }: TitleProps) => {
-  return <div>Title</div>;
+  const inputRef = useRef<HTMLInputElement>(null);
+  const update = useMutation(api.documents.update);
+
+  const [title, setTitle] = useState(initialData.title || "Untitled");
+  const [isEditing, setIsEditing] = useState(false);
+
+  const enableInput = () => {
+    setTitle(initialData.title);
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.setSelectionRange(0, inputRef.current.value.length);
+    }, 0);
+  };
+
+  return (
+    <div className="flex items-center gap-x-1">
+      {!!initialData.icon && <p>{initialData.icon}</p>}
+      {isEditing ? (
+        <Input className="h-7 px-2 focus-visible:ring-transparent" />
+      ) : (
+        <Button
+          onClick={() => {}}
+          variant="ghost"
+          size="sm"
+          className="font-normal h-auto p-1"
+        >
+          {initialData?.title}
+        </Button>
+      )}
+    </div>
+  );
 };

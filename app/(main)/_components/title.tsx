a76fd0dbe2137377d1f0c6bd13486d62231bfad6
Joby Joseph <reachjoby@gmail.com>
2024-06-22 11:48:43 -0700

    Navbar completed

--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -2,10 +2,11 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
 import { useMutation } from "convex/react";
-import { useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 
 interface TitleProps {
   initialData: Doc<"documents">;
@@ -27,14 +28,40 @@ export const Title = ({ initialData }: TitleProps) => {
     }, 0);
   };
 
+  const disableInput = () => {
+    setIsEditing(false);
+  };
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    update({
+      id: initialData._id,
+      title: event.target.value || "Untitled",
+    });
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      disableInput();
+    }
+  };
+
   return (
     <div className="flex items-center gap-x-1">
       {!!initialData.icon && <p>{initialData.icon}</p>}
       {isEditing ? (
-        <Input className="h-7 px-2 focus-visible:ring-transparent" />
+        <Input
+          ref={inputRef}
+          onClick={enableInput}
+          onBlur={disableInput}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          value={title}
+          className="h-7 px-2 focus-visible:ring-transparent"
+        />
       ) : (
         <Button
-          onClick={() => {}}
+          onClick={enableInput}
           variant="ghost"
           size="sm"
           className="font-normal h-auto p-1"
@@ -44,4 +71,8 @@ export const Title = ({ initialData }: TitleProps) => {
       )}
     </div>
   );
+};
+
+Title.Skeleton = function TitleSkeleton() {
+  return <Skeleton className="h-9 w-20 rounded-md" />;
 };

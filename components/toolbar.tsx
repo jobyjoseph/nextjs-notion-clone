85db1a4e751068e28d548034ea4253efbdc4e9d3
Joby Joseph <reachjoby@gmail.com>
2024-07-09 16:25:01 -0700

    icon remove in progress

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -49,6 +49,13 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
     }
   };
 
+  const onIconSelect = (icon: string) => {
+    update({
+      id: initialData._id,
+      icon,
+    });
+  };
+
   return (
     <div className="pl-[54px] group relative">
       {!!initialData.icon && !preview && (

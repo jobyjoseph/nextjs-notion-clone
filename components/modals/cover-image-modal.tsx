9c820e45433832e9751f05a57345ebb8ccead42b
Joby Joseph <reachjoby@gmail.com>
2024-07-20 16:01:21 -0700

    More on edgestore file upload

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -2,9 +2,35 @@
 
 import { useCoverImage } from "@/hooks/use-cover-image";
 import { Dialog, DialogContent, DialogHeader } from "../ui/dialog";
+import { SingleImageDropzone } from "@/components/single-image-dropzone";
+import { useState } from "react";
+import { useEdgeStore } from "@/lib/edgestore";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
 
 export const CoverImageModal = () => {
+  const params = useParams();
+  const update = useMutation(api.documents.update);
+  const [file, setFile] = useState<File>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const coverImage = useCoverImage();
+  const { edgestore } = useEdgeStore();
+
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmitting(true);
+      setFile(file);
+
+      const res = await edgestore.publicFiles.upload({ file });
+
+      await update({
+        id: params.documentId as Id<"documents">,
+        coverImage: res.url,
+      });
+    }
+  };
 
   return (
     <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
